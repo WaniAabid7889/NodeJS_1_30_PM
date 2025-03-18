57e01246bcf416b6421cdc4644f278fbdb68f332
abidhussain <waniabid645@gmail.com>
2025-03-18 14:39:27 +0530

    file system in react js

---
+++ file_system/index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+
+// fs.readFile(path.join(__dirname,'files','users.txt'),'utf-8',(res,err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//     console.log(res.toString());
+//     }
+// })
+
+let appendData = "\nthis is new text file we are append data";
+let user = { id: 101, name: "Hussain" };
+fs.writeFile(
+  path.join(__dirname, "files", "create.txt"),
+  JSON.stringify(user),
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("file is created");
+      if (fs.existsSync(path.join(__dirname, "files", "create.txt"))) {
+        fs.appendFile(
+          path.join(__dirname, "files", "create.txt"),
+          appendData,
+          (err) => {
+            if (err) {
+              console.log(err);
+              fs.rename(
+                path.join(__dirname, "files", "create.txt"),
+                path.join(__dirname, "files", "created.txt"),
+                (err) => {
+                  if (err) {
+                    console.error("Error renaming file:", err);
+                  } else {
+                    console.log("File renamed successfully");
+                  }
+                }
+              );
+            } else {
+              console.log("data is append in create.txt file");
+            }
+          }
+        );
+      } else {
+        console.log("file is not exist");
+      }
+    }
+  }
+);
