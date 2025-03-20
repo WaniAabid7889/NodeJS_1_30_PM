a4ad514fff80d363d25c8d70154225cecf2c3609
abidhussain <waniabid645@gmail.com>
2025-03-20 13:34:26 +0530

    Get Method create a single page demo website

--- express/index.js
+++ express/index.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 1900;
+const path = require('path');
+
+const getUser = async() =>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const result = await response.json();
+    return result;
+}
+
+
+app.get('/user',(req,res)=>{
+    let user =  getUser();
+    user.then((val)=>{
+        res.send(val);
+    }).catch((err)=>{
+        res.send(err);
+    })
+});
+
+app.get('/user1',async (req,res)=>{
+    let user =await  getUser();
+    res.send(user);
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'home.html'));
+});
+
+app.get('/header',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'view', 'header.html'));
+});
+
+app.get('/about',(req,res,next)=>{
+    next();
+    
+},(req,res)=>{ 
+    res.sendFile(path.join(__dirname, 'view', 'about.html'));
+});
+
 
 
 app.get('/',(req,res)=>{
