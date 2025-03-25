235ea2bfd3a8bf1cded413b0663b5025394c233f
abidhussain <waniabid645@gmail.com>
2025-03-25 13:32:32 +0530

    All operation crud done with express

--- express/index.js
+++ express/index.js
@@ -23,11 +23,11 @@ let users = [
   },
 ];
 
-// app.get("/user", (req, res) => {
-//   let data = req.body;
-//   console.log(data);
-//   res.send(data);
-// });
+app.get("/user", (req, res) => {
+  let data = req.query;
+  console.log(data);
+  res.send(data);
+});
 
 app.get("/user/:id?", (req, res) => {
   //params
@@ -68,6 +68,43 @@ app.post("/insert", (req, res) => {
 app.get("/", (req, res) => {
   res.send("welcome to our express js class");
 });
+
+app.put("/update/:id?", (req, res) => {
+  let id = req.params.id;
+  let data = req.body;
+  const index = users.findIndex((val) => val.id == id);
+  if (index !== -1) {
+    users[index].name = data.name;
+    users[index].address = data.address;
+    res.json({data: users , message: "User updated successfully" });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+app.delete("/delete/:id?",(req,res)=>{
+  let id = req.params.id;
+  const index = users.findIndex((val) => val.id === parseInt(id));
+  console.log(index);
+  if(index !== -1){
+    users.splice(index, 1);
+    res.json({data: users, message: "User deleted successfully"});
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+})
+
+const getUser = () =>{
+  console.log("verify this user");
+}
+app.get('/api',getUser,async(req,res)=>{
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let result = await response.json();
+  console.log(result);
+  res.json({data : result , message : "Get All users "});
+})
+
+
 
 app.listen(port, () => {
   console.log(`our website is running : http://localhost:${port}`);
