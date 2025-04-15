dfbe25b8a1ef89aa5cba106f65ac42c08265cef2
abidhussain <waniabid645@gmail.com>
2025-04-15 12:08:05 +0530

    update and delete operation complete

--- mvc2/modules/user.module.js
+++ mvc2/modules/user.module.js
@@ -15,4 +15,27 @@ async function showUser(){
     
 }
 
-module.exports ={ AddUser , showUser}
+
+async function updateUserById(data,id){
+    const {name,email,password,status} = data
+     const sql = "UPDATE users SET  name = ? , email = ? , password = ?, status = ? where id = ? ";
+     const params = [name,email,password,status,id];
+     console.log(sql," ",params);
+     const response = await db.execute(sql,params);
+     return response;
+}
+
+async function deleteUserById(id){
+    const sql = "DELETE FROM users WHERE id = ?"; 
+    const params = [id];
+    const response = await db.execute(sql, params);
+    return response;
+}
+
+
+module.exports ={
+    AddUser ,
+    showUser, 
+    updateUserById,
+    deleteUserById
+}
--- mvc2/routers/user.router.js
+++ mvc2/routers/user.router.js
@@ -14,5 +14,18 @@ module.exports = (app)=> {
         let result = await User.showUser();
         res.json({data : result[0]});
     })
+
+    router.post('/update/:id?',async(req,res)=>{
+        let id = req.params.id;
+        let data = req.body;
+        let result = await User.updateUserById(data,id);
+        res.status(200).json({data : result, message : "user update by id successfully"});
+    })
+
+    router.get('/delete/:id',async (req,res)=>{
+        let id = req.params.id;
+        let result = await User.deleteUserById(id);
+        res.status(200).json({record : result, message : "user delete successfully by id"});
+    })
     app.use('/user',router); 
 }
