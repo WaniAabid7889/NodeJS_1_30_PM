b91310e74e8135dfcd71e20148e2a9fe4d804fb7
abidhussain <waniabid645@gmail.com>
2025-04-22 12:18:36 +0530

    learn next method

--- project/server.js
+++ project/server.js
@@ -9,6 +9,34 @@ app.get('/',(req,res)=>{
     res.json({message : "welcome to project"});
 })
 
+
+app.get('/nextMethod',(req,res,next)=>{
+  next();
+  console.log(req.next)
+  res.send("After");
+},(req,res)=>{
+    console.log('before');
+})
+
+
+const A = (next,next1)=>{
+  console.log("A method")
+  next(next1)
+}
+const B = (next)=>{
+    console.log("B method")
+    next(next);
+}
+
+const C = ()=>{
+    console.log("C Method"); 
+  
+}
+
+A(B,C);
+
+app.get('/chain',[A,B,C]);
+
 app.listen(PORT, () => {
   console.log(`our server is running http://localhost:${PORT} `);
 });
